0dac9cab85ec03c7cbc8bd5063d26c9f2da1693a
Eddie Welker <welkere@ncbi.nlm.nih.gov>
2015-11-12 14:03:40 -0500

    empty

--- django-uswds/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-from . import views
-
-urlpatterns = patterns('',
-    url(r'404$', views.page_not_found, name="404"),
-    url(r'500$', views.server_error, name="500"),
-    url(r'400$', views.bad_request, name="400"),
-    url(r'403$', views.permission_denied, name="403"),
-)
